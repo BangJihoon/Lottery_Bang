370f121dcd0b472b5d0ebc8047610103efd74197
BangJihoon <jihoon289@naver.com>
2022-06-15 22:32:02 +0900

    project init

---
+++ android/app/src/main/kotlin/com/lottery_bang/lottery_bang/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lottery_bang.lottery_bang
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:lottery_bang/presentation/screens/home.dart';
+
+void main() {
+  runApp(const Home());
+}
