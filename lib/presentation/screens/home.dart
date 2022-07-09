0a47d94e448ec6c52598cbed6bdbe55453843903
BangJihoon <jihoon289@naver.com>
2022-07-09 10:12:02 +0900

    Refact : Home 화면 생성

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -9,30 +9,8 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return MaterialApp(
-      title: 'FitQA',
-      debugShowCheckedModeBanner: false,
-      /// 앱 테마 설정
-      theme: ThemeData(
-        /// 기본 글꼴
-        fontFamily: 'lottery_bang',
-        /// 기본 배경
-        backgroundColor: FColors.white,
-        scaffoldBackgroundColor: FColors.white,
-        /// 앱 바 색상
-        primaryColor: FColors.blue,
-        /// 기본 텍스트 스타일
-        textTheme:  const TextTheme(
-          bodyText1: TextStyle(
-            color: FColors.grey_6,
-            fontSize: 10.0,
-          ),
-        ),
-      ),
-      home: const Scaffold(
-          body: const LoginPage(),
-          backgroundColor: FColors.white
-      ),
+    return Container(
+      child: Text("123"),
     );
   }
 }
